c65eb777fbfb91638a7043ec8af320c0a3c28137
Redivv <j.rajca45@gmail.com>
2019-10-27 11:09:42 +0100

    Lorem Ipsum

--- resources/views/partials/gallery.blade.php
+++ resources/views/partials/gallery.blade.php
@@ -8,7 +8,7 @@
             <img src="{{asset('images/image-cap.png')}}" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">Lorem Ipsum</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, eos totam! Inventore qui debitis harum aliquid tenetur rerum nesciunt eos? Placeat nesciunt architecto cum optio enim recusandae? Sequi, molestias dolor!</p>
             </div>
             <div class="card-footer">
             <small class="text-muted">Last updated 3 mins ago</small>
@@ -18,7 +18,7 @@
             <img src="{{asset('images/image-cap.png')}}" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">Lorem Ipsum</h5>
-            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum deserunt magni accusamus ab quam commodi quis illo corporis aliquid?</p>
             </div>
             <div class="card-footer">
             <small class="text-muted">Last updated 3 mins ago</small>
@@ -28,7 +28,7 @@
             <img src="{{asset('images/image-cap.png')}}" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">Lorem Ipsum</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio iure iusto cum voluptatem nisi at odit soluta in, quo eaque inventore vero molestias suscipit ducimus blanditiis dignissimos impedit quos commodi?</p>
             </div>
             <div class="card-footer">
             <small class="text-muted">Last updated 3 mins ago</small>
