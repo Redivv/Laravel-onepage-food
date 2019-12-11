87a9b1fbbfb7ff6bf29857f49f005ab273d8856d
Redivv <j.rajca45@gmail.com>
2019-12-11 21:50:46 +0100

    Moj opis

--- resources/views/partials/products/strona_www_grafika_reklama.blade.php
+++ resources/views/partials/products/strona_www_grafika_reklama.blade.php
@@ -1,1 +1,18 @@
-<p>kek</p>
+<p>
+    Dobry programista wie, jak okiełznać technologię. Działa rzetelnie i skrupulatnie. 
+    <br>
+    Tym właśnie zajmują się nasze usługi.
+    <br><br>
+    Każdy projekt traktujemy indywidualnie, zawsze zwracamy uwagę na odpowiednią wycenę projektu. Umawiamy się na całościową cenę końcową, a zleceniodawca może śledzić postępy na bieżąco.  
+    Zapewniamy usługi wysokiej jakości w zakresie projektowania i produkcji aplikacji mobilnych i stron internetowych. Oferujemy usługi webdeweloperskie. Tworzenie nowych witryn, odświeżanie starych. Tworzymy blogi, portale internetowe, strony wizytówki. 
+    Proces opracowywania projektu oznacza dla nas nie tylko zbudowanie oprogramowania, ale także szczegółową analizę oraz pełne zaangażowanie. W ten sposób zapewniamy stabilne i unikalne rozwiązania zgodne z oczekiwaniami klienta. 
+    Zrealizowaliśmy już liczne projekty informatyczne. 
+    <br>
+    Jeśli właśnie Państwa pomysł ma być naszym następnym celem, prosimy o kontakt z nami. 
+    <br><br>
+    Jesteśmy gotowi na każde wyzwanie!
+    <br><br><br>
+    <a href="https://jakubrajca.com.pl/">Portfolio Programisty</a>
+    <br>
+    <a href="https://github.com/Redivv">GitHub</a>
+</p>
